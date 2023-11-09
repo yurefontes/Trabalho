a6ab5e2e1c69a06cc13a6dba407c3b08c7e7d330
yure fontes <yurefontes2@gmail.com>
2023-11-09 15:49:55 -0300

    Changes to be committed: 	deleted:    codigo.js 	modified:   decimo.html

--- codigo.js
+++
@@ -1,9 +0,0 @@
-<script>
-    var decimo = 1200
-    var ferias = decimo + 20%
-    
-    for (let decimo = 0; ferias < array.length; decimo++) {
-    const element = array[index];
-    
-}
-</script>
